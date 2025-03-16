e04d655ca24463058f51e9b9c2d5b89601c0660e
SeanX16 <shuhengxiahk@outlook.com>
2025-03-17 00:00:44 +0800

    add vip section_v2

--- Scripts/bilibili_json_formatted.js
+++ Scripts/bilibili_json_formatted.js
@@ -182,7 +182,7 @@ if (body) {
           */
         });
         // 删除 VIP 相关分区
-        delete v.data.vip_section_v2;
+        //delete v.data.vip_section_v2;
         //delete v.data.vip_section;
         // 清空 live_tip 和 answer
         if (v.data.hasOwnProperty("live_tip")) {
